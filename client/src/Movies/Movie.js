1a33b9c4861be3aabad43b7ab73d8fb573850718
William Schwindt <williammschwindt@gmail.com>
2020-01-19 17:48:04 -0800

    code refactored and dry

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SavedList from './SavedList';
+import MovieCard from './MovieCard';
 
 
 const Movie = (props) => {
@@ -31,27 +32,8 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
+  return(
+    <MovieCard key={movie.id} movie={movie} />
   );
 }
 
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({ movie }) => {
+  // why is { movie } needed instead of 'props'?
+  const { title, director, metascore, stars } = movie;
+  return (
+    <Link to={`/movies/${movie.id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+    </Link>
+  );
 };
 
 export default MovieCard;
